d6c1df67b1a731887e09d0bab7e0df02f7863a48
dharmeshpipariya <pipariyadharmesh@gmail.com>
2016-05-16 17:12:42 +0530

    fixed: #15 Changed behavior in side bar menu from Mouse hover to Mouse click.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,13 +1,19 @@
 ﻿$(document).ready(function () {
-    //aside toggaling on responsive
-    $(document).on('click', '.header .nav-toggle', function (e) {
-        $('.aside-menu').toggleClass('open');
-        $('body').toggleClass('aside-menu-open');
-        e.stopPropagation();
-        e.preventDefault();
-    }).on('click', function (e) {
-        if ($(e.target).closest('.aside-menu').hasClass('open')) { return; }
-        $('.aside-menu.open').removeClass('open');
-        $('body.aside-menu-open').removeClass('aside-menu-open');
-    });
+  //aside toggaling on responsive
+  $(document).on('click', '.header .nav-toggle', function (e) {
+    $('.aside-menu').toggleClass('open');
+    $('body').toggleClass('aside-menu-open');
+    e.stopPropagation();
+    e.preventDefault();
+  }).on('click', function (e) {
+    if ($(e.target).closest('.aside-menu').hasClass('open')) { return; }
+    $('.aside-menu.open').removeClass('open');
+    $('body.aside-menu-open').removeClass('aside-menu-open');
+  });
+  $('.aside-menu .main-menu').on('click', function (e) {
+    if ($(e.target).closest('li').has('ul').length) {
+      e.stopPropagation();
+      $(e.target).closest('li').toggleClass('open');
+    }
+  });
 });
